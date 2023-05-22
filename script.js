d02f06d952058c2398a61a785a4e3dc0ec219f08
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-05-22 11:53:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const element=document.createElemet('h1');
+element.innerText=Width:${window.innerWidth} and Height ${window.innerHeight}
+
+document.body.append(element);
